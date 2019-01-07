32f989236d4e678191431123e989dfe5c4d051a2
Woo-Dong <dwpark2007@naver.com>
2019-01-07 14:21:18 +0900

    init

---
+++ app.js
@@ -0,0 +1,96 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+// var sassMiddleware = require('node-sass-middleware');
+var session = require('express-session');
+var methodOverride = require('method-override');
+var flash = require('connect-flash');
+var mongoose   = require('mongoose');
+
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+
+var mongoose = require('mongoose');
+const connStr = 'mongodb://localhost/mydb1';
+mongoose.connect(connStr, {useMongoClient: true });
+mongoose.connection.on('error', console.error);
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+
+// Pug의 local에 moment라이브러리와 querystring 라이브러리를 사용할 수 있도록.
+app.locals.moment = require('moment');
+app.locals.querystring = require('querystring');
+
+
+// favicon - 웹사이트 아이콘
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// _method를 통해서 method를 변경할 수 있도록 함. PUT이나 DELETE를 사용할 수 있도록.
+app.use(methodOverride('_method', {methods: ['POST', 'GET']}));
+
+
+// app.use(sassMiddleware({
+//   src: path.join(__dirname, 'public'),
+//   dest: path.join(__dirname, 'public'),
+//   indentedSyntax: true, // true = .sass and false = .scss
+//   sourceMap: true
+// }));
+
+// session을 사용할 수 있도록.
+app.use(session({
+  resave: true,
+  saveUninitialized: true,
+  secret: 'long-long-long-secret-string-1313513tefgwdsvbjkvasd'
+}));
+
+app.use(flash()); // flash message를 사용할 수 있도록
+
+// public 디렉토리에 있는 내용은 static하게 service하도록.
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// pug의 local에 현재 사용자 정보와 flash 메시지를 전달하자.
+app.use(function(req, res, next) {
+  res.locals.currentUser = req.session.user;
+  res.locals.flashMessages = req.flash();
+  next();
+});
+
+// Route
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+
+module.exports = app;
